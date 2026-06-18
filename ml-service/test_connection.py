0876fad2a16605294a88004386c2275ab6304b30
Joel-O1 <135176241+Joel-O1@users.noreply.github.com>
2026-06-18 10:35:21 -0600

    Delete ml-service directory

--- ml-service/test_connection.py
+++
@@ -1,7 +0,0 @@
-# test_connection.py
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    conn.execute(text("SELECT 1"))
-    print("Successfully connected!")
